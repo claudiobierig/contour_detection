2b203af7a13bc440359ac913cd99d5c422888bcb
Claudio Bierig <claudiobierig@gmail.com>
2021-09-25 19:50:35 +0200

    add output dir parameter

--- contour_detection.py
+++ contour_detection.py
@@ -10,30 +10,31 @@ def convert_image(image_path, parameter):
     return cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
                 cv2.THRESH_BINARY, 11, parameter)
 
-def save_image(image, image_path, parameter):
+def save_image(image, image_path, output_dir, parameter):
     (input_dir, input_file_name) = os.path.split(image_path)
     (input_file_name_without_extension, input_file_extension) = os.path.splitext(input_file_name)
     
     try:
-        os.mkdir(os.path.join(input_dir, "out"))
+        os.mkdir(output_dir)
     except:
         pass
     cv2.imwrite(
         os.path.join(
-            input_dir, "out", f"{input_file_name_without_extension}_{parameter}{input_file_extension}"
+            output_dir, f"{input_file_name_without_extension}_{parameter}{input_file_extension}"
         ),
         image
-    ) 
+    )
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Count number of words of the input')
     parser.add_argument("--input_files", help="Specify an input picture.", nargs='+', default=["thumbs_up_down.jpg"])
+    parser.add_argument("--output_dir", help="Folder in which to save the converted images.", default="out")
     parser.add_argument("--parameters", help="Threshold for contour detection", nargs='+', default=[20], type=int)
 
     arguments = parser.parse_args()
 
     for file, parameter in itertools.product(arguments.input_files, arguments.parameters):
         image = convert_image(file, parameter)
-        save_image(image, file, parameter)
+        save_image(image, file, arguments.output_dir, parameter)
     
     
